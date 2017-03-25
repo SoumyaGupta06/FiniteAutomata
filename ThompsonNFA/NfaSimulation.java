deeb3cb6792e9a2bb064e51a4a91288b8beb3d6b
SoumyaGupta06 <soumyagupta0610@gmail.com>
2017-03-26 00:55:55 +0530

    Update NfaSimulation.java

--- ThompsonNFA/NfaSimulation.java
+++ ThompsonNFA/NfaSimulation.java
@@ -19,8 +19,8 @@ public class NfaSimulation {
     
     public static boolean match(State startstate, String input){
         
-        ArrayList <State> cList = new ArrayList<State> ();
-        ArrayList <State> nList = new ArrayList<State> ();
+        ArrayList <State> cList = new ArrayList<State> ();  //the current set of states that the NFA is in
+        ArrayList <State> nList = new ArrayList<State> ();  //the next set of states that the NFA will be in after processing the current character
         int listID = 0;
         addState(cList, startstate, listID);
         for (int i = 0; i < input.length(); i++){
@@ -32,7 +32,7 @@ public class NfaSimulation {
         return isMatch(cList);
         
     }
-    
+    //check if the final state list contains the matching state
     public static boolean isMatch(ArrayList<State> list){
         for (int i = 0; i < list.size(); i++){
             State s = list.get(i);
@@ -42,6 +42,8 @@ public class NfaSimulation {
         }
         return false;
     }
+    
+    //advances the NFA past a single character, using the current list clist to compute the next list nlist
     private static int step(ArrayList<State> currentList, char c, ArrayList<State> nextList, int listID){
         listID++;
         for (int i = 0; i < currentList.size(); i++){
@@ -52,6 +54,8 @@ public class NfaSimulation {
         }
         return listID;
     }
+    
+    //adds a state to the list, but not if it is already on the list
     private static void addState(ArrayList<State> list, State s, int listID){
         if (s == null || s.getLastlist() == listID)
             return;
